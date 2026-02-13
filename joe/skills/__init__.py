2ffb7428d1926bf254cbeba6320e22449f3766f3
Marko Antić <marko@prodit.rs>
2026-02-13 04:02:23 +0100

    Delete joe directory

--- joe/skills/__init__.py
+++
@@ -1,6 +0,0 @@
-from .time_date import TimeDateSkill
-from .system_status import SystemStatusSkill
-from .memory_skill import MemorySkill
-from .facts import FactsSkill
-from .voice import VoiceSkill
-from .mode import ModeSkill
